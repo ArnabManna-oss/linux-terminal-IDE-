870a029232c60f9c6f985d4cf5333c50fe5a66df
Arnab Manna <74395720+ArnabManna-oss@users.noreply.github.com>
2022-07-29 01:14:20 +0530

    Update Shell_vim_IED.sh.sh
    fix.update

--- Shell_vim_IED.sh.sh
+++ Shell_vim_IED.sh.sh
@@ -2,12 +2,12 @@ echo -ne "enter a file name :"
 read f1
 touch $f1
 chmod +x $f1
-vi $f1
+nano $f1
 echo -ne "do you want to execute the file (y/n) :"
 read en1
 case "$en1" in
         #case 1
-        "y") ./$f1 ;;
+        "y") bash $f1 ;;
 
         #case 2
         "n") echo "process ends" ;;
